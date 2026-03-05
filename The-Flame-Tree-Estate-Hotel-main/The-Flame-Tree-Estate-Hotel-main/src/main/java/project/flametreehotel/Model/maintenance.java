23a1b33c6ecfa812f6b18f69b25c630ae9a79e5a
IT24201063 <IT24201063@my.sliit.lk>
2026-03-05 20:01:14 +0530

    Add files via upload

---
+++ The-Flame-Tree-Estate-Hotel-main/The-Flame-Tree-Estate-Hotel-main/src/main/java/project/flametreehotel/Model/maintenance.java
@@ -0,0 +1,22 @@
+package project.flametreehotel.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class maintenance {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private int id;
+    private String ticket;
+    private String location;
+    private String issue;
+    private String assignedTo;
+
+    private String status;
+
+    private boolean approved;
+}
